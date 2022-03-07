ccc3d4dabe8c83771b5b2ff7287f4f823706e64b
Skeleton Crew <skeletoncrewrip@gmail.com>
2022-03-07 10:54:29 -0500

    Remove refresh button

--- app/gem-farm/src/components/gem-farm/FarmerDisplay.vue
+++ app/gem-farm/src/components/gem-farm/FarmerDisplay.vue
@@ -1,11 +1,13 @@
 <template>
 <div>
+  <!--
   <div class="absolute top-5 right-5 primary px-4 py-2 rounded-full bg-indigo-500 cursor-pointer" @click="refreshFarmer">
     <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-5 -mt-1 inline-block text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
       <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15" />
     </svg>
     <span class="text-white inline-block ml-1">Refresh</span>
   </div>
+  -->
 
   <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 text-sm text-center mb-4">
 
